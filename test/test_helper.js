84557219cf02f3128520d981aa299bd7ad84830d
Eric Wang <skygragon@gmail.com>
2016-08-28 12:01:21 +0800

    Add dummy ut to let test pass.
    Signed-off-by: Eric Wang <skygragon@gmail.com>

---
+++ test/test_helper.js
@@ -0,0 +1,10 @@
+var assert = require('chai').assert;
+
+var h = require('../lib/helper');
+
+describe('helper', function() {
+  describe('#dummy', function() {
+    it('should ok', function() {
+    });
+  });
+});
