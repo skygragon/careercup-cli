acebc206cd6324c51de0c8a1edd63b53910fc08f
Eric Wang <skygragon@gmail.com>
2016-08-28 11:50:29 +0800

    Add README.md
    Signed-off-by: Eric Wang <skygragon@gmail.com>

---
+++ .c3-completion.bash
@@ -0,0 +1,29 @@
+###-begin-c3-completions-###
+#
+# yargs command completion script
+#
+# Installation: bin/c3 completion >> ~/.bashrc
+#    or bin/c3 completion >> ~/.bash_profile on OSX.
+#
+_yargs_completions()
+{
+    local cur_word args type_list
+
+    cur_word="${COMP_WORDS[COMP_CWORD]}"
+    args=("${COMP_WORDS[@]}")
+
+    # ask yargs to generate completions.
+    type_list=$(bin/c3 --get-yargs-completions "${args[@]}")
+
+    COMPREPLY=( $(compgen -W "${type_list}" -- ${cur_word}) )
+
+    # if no match was found, fall back to filename completion
+    if [ ${#COMPREPLY[@]} -eq 0 ]; then
+      COMPREPLY=( $(compgen -f -- "${cur_word}" ) )
+    fi
+
+    return 0
+}
+complete -F _yargs_completions c3
+###-end-c3-completions-###
+
